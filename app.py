b183db419a6d27f361699c3149ccb9e38a40877f
Ahmed_Hossam <61332730+Ahmed-Hossam-Aldeen@users.noreply.github.com>
2023-10-28 03:32:24 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,48 @@
+# app.py
+import streamlit as st
+import os
+
+# Clone the repository and navigate to the roop folder
+os.system("git clone https://github.com/based9based/roop")
+os.chdir("roop")
+
+# Install requirements
+os.system("pip install -r requirements.txt")
+
+# Download the model file
+os.system("wget https://civitai.com/api/download/models/85159 -O inswapper_128.onnx")
+
+# Import required modules
+from PIL import Image
+import io
+import subprocess
+
+# Streamlit app header
+st.title("Face Swapper Streamlit App")
+
+# Upload source and target images
+source_image = st.file_uploader("Upload source image (AS.jpg)", type=["jpg", "png"])
+target_video = st.file_uploader("Upload target video (NSF.mp4)", type=["mp4"])
+
+# Run the face swapping and enhancement process
+if source_image and target_video:
+    st.info("Processing... This may take a while.")
+    
+    # Save uploaded files
+    source_path = "source.jpg"
+    target_path = "target.mp4"
+    source_image.save(source_path)
+    target_video.save(target_path)
+    
+    # Run face swapping and enhancement
+    command = f"python run.py --target {target_path} --source {source_path} -o swapped.mp4 --execution-provider cuda --frame-processor face_swapper"
+    subprocess.run(command, shell=True)
+    
+    # Display processed video
+    processed_video = open("swapped.mp4", "rb")
+    video_bytes = processed_video.read()
+    st.video(video_bytes)
+    st.success("Processing complete!")
+
+# Note to the user
+st.info("Note: Make sure to upload a source image and a target video to perform the face swapping and enhancement.")
